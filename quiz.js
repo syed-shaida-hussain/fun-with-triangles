f18fca942c79675c0e69ef602fc8b20b94785e2a
syed shaida <syedshaida295@gmail.com>
2021-08-31 10:13:35 +0530

    added triangle quiz

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitFormBtn = document.querySelector("#submit-form-btn");
+const outputBox = document.querySelector("#output");
+const correctAnswers = ["90°" , "right angled"];
+
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score++;
+        }
+        index++;
+    }
+    outputBox.innerText = "your score is " + score;
+}
+
+submitFormBtn.addEventListener("click", calculateScore);
+
